d9afd80a584eb34afe53cbffd36800314f64374d
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-22 10:22:59 +0700

    Fineale Stage 1

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject Gate, Transision;
+
+    void OnCollisionEnter(Collision gate)
+    {
+        if(gate.gameObject.name == "Cube (1)")
+        {
+            Debug.Log("Gate");
+            Gate.SetActive(false);
+            Transision.SetActive(true);
+            Destroy(gate.gameObject);
+        }
+
+
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,7 +7,7 @@ public class Controller : MonoBehaviour
 {
     public GameObject cameraOld, cameraNew, pannelWin, pannelLoose, pintu1, pintu2;
 
-    public Animator animLaser;
+    public Animator gantiScene;
 
 
     // Update is called once per frame
@@ -40,6 +40,7 @@ public class Controller : MonoBehaviour
             if (Coll.gameObject.name == "VentCover")
             {
                 Debug.Log("Vent");
+                GantiScence();
                 Destroy(Coll.gameObject);
             }
 
@@ -55,13 +56,58 @@ public class Controller : MonoBehaviour
                 Destroy(pintu1);
                 Destroy(pintu2);
             }
+
+            if (Coll.gameObject.name == "SummonStage2")
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+
         } else
         if (SceneManager.GetActiveScene().name == "1-1")
         {
 
-         
+            if (Coll.collider.CompareTag("papan"))
+            {
+                Destroy(GameObject.FindWithTag("papan"));
+            }
+
+            if (Coll.collider.CompareTag("katanyaAir"))
+            {
+                SceneManager.LoadScene("1-1");
+                Debug.Log("Lab2");
+            }
+
+            if (Coll.collider.CompareTag("Lift"))
+            {
+                GantiScence();
+            }
+
+
+
+        }else if ( SceneManager.GetActiveScene().name == "Level 2")
+        {
+            if (Coll.collider.CompareTag("Tuas"))
+            {
+                Debug.Log("testing");
+            }
         }
 
+    }
+
+    public void GantiScence()
+    {
+
+        StartCoroutine(Ganti(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+
+    IEnumerator Ganti (int level)
+    {
+        gantiScene.SetTrigger("Ganti");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(level);
     }
 
 }
